7cbb8e6c0216a5f0d42ef70a761714a55b4dcf9d
Abdallah ANRICHIDINE <zerodevsn@gmail.com>
2024-09-19 11:18:30 +0000

    Intégration de React bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-import { Home } from "./pages/home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Home } from "./pages/home.jsx";
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+
+export function Home(props) {
+  return (
+    <div id="home">
+      <div className="container">
+        <h1>Contact Manager App</h1>
+      </div>
+    </div>
+  );
+}
